9cf72989dc595b17abbc5addcdc02425628e0e2b
interface <1070146808@qq.com>
2023-07-13 07:34:34 +0800

    测试eclipse创建仓库 push文件的做法,这个app用于eureka server

---
+++ src/main/java/com/bendizhengdao/MicroserviceEurekaConsumerMovieApplication.java
@@ -0,0 +1,15 @@
+package com.bendizhengdao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MicroserviceEurekaConsumerMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceEurekaConsumerMovieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bendizhengdao/MicroserviceEurekaConsumerMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bendizhengdao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceEurekaConsumerMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
